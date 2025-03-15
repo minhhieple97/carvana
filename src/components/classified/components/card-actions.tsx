74b250b8195388995d7c45b3857da4d6b23e05a3
hiep-le_fmgl <ext.hiep.le@softwareone.com>
2025-03-15 15:13:49 +0700

    feat: Improve card-action

--- src/components/classified/components/card-actions.tsx
+++ src/components/classified/components/card-actions.tsx
@@ -3,20 +3,43 @@ import { Button } from '@/components/ui/button';
 import { routes } from '@/config/routes';
 import { MultiStepFormEnum } from '@/config/types';
 import { CardActionsProps } from '../types';
+import { CalendarDays, ArrowRight } from 'lucide-react';
 
 export const CardActions = ({ slug }: CardActionsProps) => {
   return (
     <div className="mt-4 grid grid-cols-2 gap-3">
       <Button
-        className="w-full transition-colors hover:bg-primary/90"
+        className="group w-full bg-white border-primary text-primary hover:bg-primary/5 
+                   hover:border-primary/90 transition-all duration-300"
         asChild
         variant="outline"
         size="sm"
       >
-        <Link href={routes.reserve(slug, MultiStepFormEnum.WELCOME)}>Reserve</Link>
+        <Link
+          href={routes.reserve(slug, MultiStepFormEnum.WELCOME)}
+          className="flex items-center justify-center gap-2"
+        >
+          <CalendarDays className="w-4 h-4" />
+          <span>Reserve</span>
+        </Link>
       </Button>
-      <Button className="w-full transition-colors hover:bg-primary/90" asChild size="sm">
-        <Link href={routes.singleClassified(slug)}>View Details</Link>
+
+      <Button
+        className="group w-full bg-primary hover:bg-primary/90 
+                   transition-all duration-300"
+        asChild
+        size="sm"
+      >
+        <Link
+          href={routes.singleClassified(slug)}
+          className="flex items-center justify-center gap-2"
+        >
+          <span>View Details</span>
+          <ArrowRight
+            className="w-4 h-4 transition-transform duration-300 
+                                group-hover:translate-x-1"
+          />
+        </Link>
       </Button>
     </div>
   );
