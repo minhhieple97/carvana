ff7ccdbd1d5b4103d542d45e00560247411ffdff
minhhieple97 <hieplevuc@gmail.com>
2025-03-17 14:11:22 +0700

    feat: Query classifields

--- src/features/classifieds/server/db/classifieds.ts
+++ src/features/classifieds/server/db/classifieds.ts
@@ -1,21 +1,24 @@
+import { AwaitedPageProps } from '@/config/types';
+import { PageSchema } from '@/app/schemas/page.schema';
 import { prisma } from '@/lib/prisma';
-import { redis } from '@/lib/redis-store';
 import { unstable_noStore as noStore } from 'next/cache';
-import type { Favourites } from '@/config/types';
+import { CLASSIFIEDS_PER_PAGE } from '@/config/constants';
 
-export async function getCount() {
+export const getCount = async (searchParams: AwaitedPageProps['searchParams']) => {
   noStore();
   return prisma.classified.count();
-}
+};
 
-export async function getClassifieds() {
+export const getClassifieds = async (searchParams: AwaitedPageProps['searchParams']) => {
   noStore();
+  const validPage = PageSchema.parse(searchParams.page);
+  const page = validPage ? validPage : 1;
+
+  const offset = (page - 1) * CLASSIFIEDS_PER_PAGE;
+
   return prisma.classified.findMany({
-    include: {
-      images: true,
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
+    include: { images: { take: 1 } },
+    skip: offset,
+    take: CLASSIFIEDS_PER_PAGE,
   });
-}
+};
