b176e31960a8ff22efd83a256b261e2c917d6c1f
minhhieple97 <hieplevuc@gmail.com>
2025-03-17 14:11:34 +0700

    chore: Format card action

--- src/features/classifieds/components/card-actions.tsx
+++ src/features/classifieds/components/card-actions.tsx
@@ -10,8 +10,7 @@ import type { CardActionsProps } from '../types';
 export const CardActions = ({ slug }: CardActionsProps) => (
   <div className="mt-4 grid grid-cols-2 gap-3">
     <Button
-      className="group w-full bg-white border-primary text-primary hover:bg-primary/5 
-                   hover:border-primary/90 transition-all duration-300"
+      className="group w-full bg-white border-primary text-primary hover:bg-primary/5 hover:border-primary/90 transition-all duration-300"
       asChild
       variant="outline"
       size="sm"
@@ -26,17 +25,13 @@ export const CardActions = ({ slug }: CardActionsProps) => (
     </Button>
 
     <Button
-      className="group w-full bg-primary hover:bg-primary/90 
-                   transition-all duration-300"
+      className="group w-full bg-primary hover:bg-primary/90 transition-all duration-300"
       asChild
       size="sm"
     >
       <Link href={routes.singleClassified(slug)} className="flex items-center justify-center gap-2">
         <span>View Details</span>
-        <ArrowRight
-          className="w-4 h-4 transition-transform duration-300 
-                                group-hover:translate-x-1"
-        />
+        <ArrowRight className="w-4 h-4 transition-transform duration-300 group-hover:translate-x-1" />
       </Link>
     </Button>
   </div>
