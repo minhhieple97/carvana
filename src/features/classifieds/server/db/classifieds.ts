f040a5e4aeb42066da9fe17e401e43db112e1d17
minhhieple97 <hieplevuc@gmail.com>
2025-03-19 20:59:38 +0700

    feat: Integrate classified filter query in getClassifieds function and improve searchParams handling

--- src/features/classifieds/server/db/classifieds.ts
+++ src/features/classifieds/server/db/classifieds.ts
@@ -1,8 +1,9 @@
 import { AwaitedPageProps } from '@/config/types';
-import { PageSchema } from '@/app/schemas/page.schema';
+import { PageSchema } from '@/app/schemas';
 import { prisma } from '@/lib/prisma';
 import { unstable_noStore as noStore } from 'next/cache';
 import { CLASSIFIEDS_PER_PAGE } from '@/config/constants';
+import { buildClassifiedFilterQuery } from '../services/classifieds';
 
 export const getCount = async (searchParams: AwaitedPageProps['searchParams']) => {
   noStore();
@@ -11,12 +12,14 @@ export const getCount = async (searchParams: AwaitedPageProps['searchParams']) =
 
 export const getClassifieds = async (searchParams: AwaitedPageProps['searchParams']) => {
   noStore();
-  const validPage = PageSchema.parse(searchParams.page);
+  const validPage = PageSchema.parse(searchParams?.page);
+
   const page = validPage ? validPage : 1;
 
   const offset = (page - 1) * CLASSIFIEDS_PER_PAGE;
 
   return prisma.classified.findMany({
+    where: buildClassifiedFilterQuery(searchParams),
     include: { images: { take: 1 } },
     skip: offset,
     take: CLASSIFIEDS_PER_PAGE,
