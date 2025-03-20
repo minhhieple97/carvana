9781f59f9002c08ada348fb1931c7e709a780080
minhhieple97 <hieplevuc@gmail.com>
2025-03-20 20:52:34 +0700

    refactor: Extract sidebar logic into a custom hook and streamline component by utilizing useSidebar for improved state management and readability

--- src/features/classifieds/components/sidebar.tsx
+++ src/features/classifieds/components/sidebar.tsx
@@ -9,13 +9,9 @@ import {
   Transmission,
   ULEZCompliance,
 } from '@prisma/client';
-import { useRouter } from 'next/navigation';
-import { parseAsString, useQueryStates } from 'nuqs';
-import { type ChangeEvent, useEffect, useState } from 'react';
 
 import { SearchInput, RangeFilter, TaxonomyFilters } from '@/components/shared';
-import { routes } from '@/config/routes';
-import { env } from '@/env';
+
 import {
   cn,
   formatBodyType,
@@ -27,68 +23,14 @@ import {
 } from '@/lib/utils';
 
 import type { SidebarProps } from '@/config/types';
+import { Select } from '@/components/ui';
+import { useSidebar } from '../hooks/useSidebar';
 
 export const Sidebar = ({ minMaxValues, searchParams }: SidebarProps) => {
-  const router = useRouter();
-  const [filterCount, setFilterCount] = useState(0);
   const { _min, _max } = minMaxValues;
-  const [queryStates, setQueryStates] = useQueryStates(
-    {
-      make: parseAsString.withDefault(''),
-      model: parseAsString.withDefault(''),
-      modelVariant: parseAsString.withDefault(''),
-      minYear: parseAsString.withDefault(''),
-      maxYear: parseAsString.withDefault(''),
-      minPrice: parseAsString.withDefault(''),
-      maxPrice: parseAsString.withDefault(''),
-      minReading: parseAsString.withDefault(''),
-      maxReading: parseAsString.withDefault(''),
-      currency: parseAsString.withDefault(''),
-      odoUnit: parseAsString.withDefault(''),
-      transmission: parseAsString.withDefault(''),
-      fuelType: parseAsString.withDefault(''),
-      bodyType: parseAsString.withDefault(''),
-      colour: parseAsString.withDefault(''),
-      doors: parseAsString.withDefault(''),
-      seats: parseAsString.withDefault(''),
-      ulezCompliance: parseAsString.withDefault(''),
-    },
-    {
-      shallow: false,
-    }
-  );
-
-  useEffect(() => {
-    const filterCount = Object.entries(searchParams as Record<string, string>).filter(
-      ([key, value]) => key !== 'page' && value
-    ).length;
-
-    setFilterCount(filterCount);
-  }, [searchParams]);
-
-  const clearFilters = () => {
-    const url = new URL(routes.classifieds, env.NEXT_PUBLIC_APP_URL);
-    window.location.replace(url.toString());
-    setFilterCount(0);
-  };
-
-  const handleChange = async (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-    const { name, value } = e.target;
-
-    setQueryStates({
-      [name]: value || null,
-    });
-
-    if (name === 'make') {
-      setQueryStates({
-        model: null,
-        modelVariant: null,
-      });
-    }
-
-    router.refresh();
-  };
-
+  const { filterCount, clearFilters, handleChange, queryStates } = useSidebar({
+    searchParams,
+  });
   return (
     <aside className="w-[21.25rem] overflow-hidden rounded-xl border border-gray-100 bg-white shadow-md">
       <div className="sticky top-4">
@@ -118,7 +60,131 @@ export const Sidebar = ({ minMaxValues, searchParams }: SidebarProps) => {
         <div className="max-h-[calc(100vh-12rem)] space-y-7 overflow-y-auto p-5">
           <TaxonomyFilters searchParams={searchParams} handleChange={handleChange} />
 
-          {/* Uncomment and update other filters as needed */}
+          <RangeFilter
+            label="Year"
+            minName="minYear"
+            maxName="maxYear"
+            defaultMin={_min.year || 1925}
+            defaultMax={_max.year || new Date().getFullYear()}
+            handleChange={handleChange}
+            searchParams={searchParams}
+          />
+          <RangeFilter
+            label="Price"
+            minName="minPrice"
+            maxName="maxPrice"
+            defaultMin={_min.price || 0}
+            defaultMax={_max.price || 21474836}
+            handleChange={handleChange}
+            searchParams={searchParams}
+            increment={1000000}
+            thousandSeparator
+            currency={{
+              currencyCode: 'GBP',
+            }}
+          />
+          <RangeFilter
+            label="Odometer Reading"
+            minName="minReading"
+            maxName="maxReading"
+            defaultMin={_min.odoReading || 0}
+            defaultMax={_max.odoReading || 1000000}
+            handleChange={handleChange}
+            searchParams={searchParams}
+            increment={5000}
+            thousandSeparator
+          />
+          <Select
+            label="Currency"
+            name="currency"
+            value={queryStates.currency || ''}
+            onChange={handleChange}
+            options={Object.values(CurrencyCode).map((value) => ({
+              label: value,
+              value,
+            }))}
+          />
+          <Select
+            label="Odometer Unit"
+            name="odoUnit"
+            value={queryStates.odoUnit || ''}
+            onChange={handleChange}
+            options={Object.values(OdoUnit).map((value) => ({
+              label: formatOdometerUnit(value),
+              value,
+            }))}
+          />
+          <Select
+            label="Transmission"
+            name="transmission"
+            value={queryStates.transmission || ''}
+            onChange={handleChange}
+            options={Object.values(Transmission).map((value) => ({
+              label: formatTransmission(value),
+              value,
+            }))}
+          />
+          <Select
+            label="Fuel Type"
+            name="fuelType"
+            value={queryStates.fuelType || ''}
+            onChange={handleChange}
+            options={Object.values(FuelType).map((value) => ({
+              label: formatFuelType(value),
+              value,
+            }))}
+          />
+          <Select
+            label="Body Type"
+            name="bodyType"
+            value={queryStates.bodyType || ''}
+            onChange={handleChange}
+            options={Object.values(BodyType).map((value) => ({
+              label: formatBodyType(value),
+              value,
+            }))}
+          />
+          <Select
+            label="Colour"
+            name="colour"
+            value={queryStates.colour || ''}
+            onChange={handleChange}
+            options={Object.values(Colour).map((value) => ({
+              label: formatColour(value),
+              value,
+            }))}
+          />
+          <Select
+            label="ULEZ Compliance"
+            name="ulezCompliance"
+            value={queryStates.ulezCompliance || ''}
+            onChange={handleChange}
+            options={Object.values(ULEZCompliance).map((value) => ({
+              label: formatUlezCompliance(value),
+              value,
+            }))}
+          />
+
+          <Select
+            label="Doors"
+            name="doors"
+            value={queryStates.doors || ''}
+            onChange={handleChange}
+            options={Array.from({ length: 6 }).map((_, i) => ({
+              label: Number(i + 1).toString(),
+              value: Number(i + 1).toString(),
+            }))}
+          />
+          <Select
+            label="Seats"
+            name="seats"
+            value={queryStates.seats || ''}
+            onChange={handleChange}
+            options={Array.from({ length: 8 }).map((_, i) => ({
+              label: Number(i + 1).toString(),
+              value: Number(i + 1).toString(),
+            }))}
+          />
         </div>
       </div>
     </aside>
