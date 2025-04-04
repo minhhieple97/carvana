8d522a4eb618305302399558c88143b96f838eda
hiep-le_fmgl <hieplevuc@gmail.com>
2025-04-04 09:45:28 +0700

    refactor: Update classifiedPlaceholder image source to use environment variable for dynamic URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,8 +1,9 @@
+import { env } from '@/env';
+
 import { routes } from './routes';
 
 export const imageSources = {
-  classifiedPlaceholder:
-    'https://majestic-motors.s3.eu-west-2.amazonaws.com/uploads/classified-placeholder.jpeg',
+  classifiedPlaceholder: `${env.NEXT_PUBLIC_IMGIX_URL}/uploads/AdobeStock_855683950.jpeg`,
   carLinup: 'https://majestic-motors.s3.eu-west-2.amazonaws.com/uploads/car-lineup.jpeg',
   featureSection: 'https://majestic-motors.s3.eu-west-2.amazonaws.com/uploads/feature-section.jpg',
 };
