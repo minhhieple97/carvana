0eca8f88d9b951841f9c0b23de6f032535804bdf
hiep-le_fmgl <hieplevuc@gmail.com>
2025-04-04 16:25:25 +0700

    feat: Create HomepageTaxonomyFilters component for managing filters on the homepage

---
+++ src/features/homepage/components/homepage-filters.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { RangeFilter } from '@/components/shared/range-filter';
+import { TaxonomyFilters } from '@/components';
+import type { SidebarProps } from '@/config/types';
+import { useHomepageFilters } from '../hooks/useHomepageFilters';
+
+interface HomepageTaxonomyFiltersProps extends SidebarProps {}
+
+export const HomepageTaxonomyFilters = ({
+  searchParams,
+  minMaxValues,
+}: HomepageTaxonomyFiltersProps) => {
+  const { _min, _max } = minMaxValues;
+
+  const { handleFilterChange } = useHomepageFilters();
+
+  return (
+    <>
+      <TaxonomyFilters handleChange={handleFilterChange} searchParams={searchParams} />
+      <RangeFilter
+        label="Year"
+        minName="minYear"
+        maxName="maxYear"
+        defaultMin={_min.year || 1925}
+        defaultMax={_max.year || new Date().getFullYear()}
+        handleChange={handleFilterChange}
+        searchParams={searchParams}
+      />
+      <RangeFilter
+        label="Price"
+        minName="minPrice"
+        maxName="maxPrice"
+        defaultMin={_min.price || 0}
+        defaultMax={_max.price || 21474836}
+        handleChange={handleFilterChange}
+        searchParams={searchParams}
+        increment={1000000}
+        thousandSeparator
+        currency={{
+          currencyCode: 'GBP',
+        }}
+      />
+    </>
+  );
+};
