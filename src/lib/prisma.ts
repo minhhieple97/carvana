46f38d953f779c791218596c2e6adbf2c3909bd4
hiep-le_fmgl <hieplevuc@gmail.com>
2025-04-09 08:54:04 +0700

    feat: Add @prisma/extension-accelerate for performance optimization and update PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,10 @@
 'server-only';
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaClient } from '@prisma/client/edge';
+import { withAccelerate } from '@prisma/extension-accelerate';
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 function makeClient() {
-  return new PrismaClient();
+  return new PrismaClient().$extends(withAccelerate());
 }
 
 export const prisma = globalForPrisma.prisma || makeClient();
