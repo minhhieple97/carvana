9e1c9ea7629cb26d29031e6bd96789b19fd09f80
hiep-le_fmgl <hieplevuc@gmail.com>
2025-04-18 20:29:45 +0700

    feat: Add ClassifiedsTableRow component for displaying individual classified items in a table

---
+++ src/features/classifieds/components/classified-table-row.tsx
@@ -0,0 +1,46 @@
+import { formatClassifiedStatus, formatColour, formatPrice } from '@/lib/utils';
+import { format } from 'date-fns';
+import Image from 'next/image';
+
+import { ActionButtons } from './action-buttons';
+import { Badge, TableCell, TableRow } from '@/components';
+import { ClassifiedBadgeMap, ClassifiedWithImages } from '../types';
+
+export const ClassifiedsTableRow = (classified: ClassifiedWithImages) => {
+  return (
+    <TableRow className="text-muted/75 border-white/5 hover:bg-primary-300">
+      <TableCell className="font-medium">{classified.id}</TableCell>
+      <TableCell className="p-0">
+        <Image
+          src={classified.images[0].src}
+          alt={classified.images[0].alt}
+          width={120}
+          height={80}
+          quality={1}
+          className="aspect-3/2 object-cover rounded p-0.5"
+        />
+      </TableCell>
+      <TableCell className="hidden md:table-cell">{classified.title}</TableCell>
+      <TableCell className="hidden md:table-cell">
+        {formatPrice({
+          price: classified.price,
+          currency: classified.currency,
+        })}
+      </TableCell>
+      <TableCell className="hidden md:table-cell">{classified.vrm}</TableCell>
+      <TableCell className="hidden md:table-cell">{formatColour(classified.colour)}</TableCell>
+      <TableCell className="hidden md:table-cell">
+        <Badge variant={ClassifiedBadgeMap[classified.status]}>
+          {formatClassifiedStatus(classified.status)}
+        </Badge>
+      </TableCell>
+      <TableCell className="hidden md:table-cell">
+        {format(classified.createdAt, 'do MMM yyy HH:mm')}
+      </TableCell>
+      <TableCell>{classified.views}</TableCell>
+      <TableCell className="flex gap-x-2">
+        <ActionButtons classified={classified} />
+      </TableCell>
+    </TableRow>
+  );
+};
