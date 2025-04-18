6105f77088b9ee5b9b456cd6098711ee5b7ff4bc
hiep-le_fmgl <hieplevuc@gmail.com>
2025-04-18 13:24:12 +0700

    feat: Enhance authentication actions with admin role validation and improved error handling

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,3 +1,4 @@
+import { Role } from '@prisma/client';
 import { cookies } from 'next/headers';
 import { createSafeActionClient, DEFAULT_SERVER_ERROR_MESSAGE } from 'next-safe-action';
 
@@ -20,11 +21,20 @@ export const authAction = action.use(async ({ next }) => {
   const session = cookieStore.get('session')?.value;
 
   if (!session) {
-    throw new Error('Session not found!');
+    throw new ActionError('Session not found!');
   }
   const user = await getUserFromSession(cookieStore);
   if (!user) {
-    throw new Error('Session is not valid!');
+    throw new ActionError('Session is not valid!');
   }
   return next({ ctx: { user } });
+});
+
+export const adminAction = authAction.use(async ({ next, ctx }) => {
+  const userRole = ctx.user.role;
+
+  if (userRole !== Role.admin) {
+    throw new ActionError('Invalid action');
+  }
+  return next({ ctx });
 });
